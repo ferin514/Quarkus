ee7983cc005d3819cf6cd5def87a4d91a6efce77
meenakshi sajeevan <meenakshisajeevan2000@gmail.com>
2023-06-06 10:30:42 +0530

    first commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+-- This file allow to write SQL commands that will be emitted in test and dev.
+-- The commands are commented as their support depends of the database
+ insert into Person (id, firstName,lastName) values(1, 'abu','bekkar');
+ insert into Person (id, firstName,lastName) values(2, 'sam','emmanual');
+ insert into Person (id, firstName,lastName) values(3, 'helma','jose');
+ insert into Person (id, firstName,lastName) values(4, 'criz','simon');
+-- alter sequence myentity_seq restart with 4;
